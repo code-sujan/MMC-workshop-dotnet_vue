285553e7061b25b24737def815addf68445c4fba
code-sujan <asmitment@gmail.com>
2023-05-27 11:29:31 +0545

    feat: add update in classinfo

--- Controllers/ClassInfoController.cs
+++ Controllers/ClassInfoController.cs
@@ -18,7 +18,7 @@ public class ClassInfoController : Controller
     [HttpGet]
     public async Task<IActionResult> Index()
     {
-        var list = await _context.ClassInfos.ToListAsync();
+        var list = await _context.ClassInfos.OrderBy(x => x.Id).ToListAsync();
         return View(list);
     }
 
@@ -48,4 +48,45 @@ public class ClassInfoController : Controller
             return BadRequest(e.Message);
         }
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Update(long id)
+    {
+        try
+        {
+            var info = await _context.ClassInfos.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (info == null) throw new Exception($"Class info with id {id} not found");
+            var model = new ClassInfoVm()
+            {
+                Name = info.Name,
+                Description = info.Description
+            };
+            return View(model);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(long id, ClassInfoVm model)
+    {
+        try
+        {
+            var info = await _context.ClassInfos.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (info == null) throw new Exception($"Class info with id {id} not found");
+            info.Name = model.Name;
+            info.Description = model.Description;
+            _context.ClassInfos.Update(info);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    
 }
